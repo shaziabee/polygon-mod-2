25c95c2ed0a45c739fd5eef723a5d24ebd74fba2
Shazia Bee <shaziabee9027@gmail.com>
2024-07-05 17:31:15 +0530

    Update hardhat.config.ts

--- zardkat-main/hardhat.config.ts
+++ zardkat-main/hardhat.config.ts
@@ -3,8 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 // https://github.com/projectsophon/hardhat-circom
 import "hardhat-circom";
 // circuits
-import circuits = require('./circuits.config.json');
-import 'dotenv/config';
+import circuits = require('./circuits.config.json')
 
 // set env var to the root of the project
 process.env.BASE_PATH = __dirname;
@@ -25,12 +24,12 @@ const config: HardhatUserConfig = {
   },
   networks: {
     sepolia: {
-      url: 'https://rpc.sepolia.org',
-      accounts: [process.env.Private_key!],
-    },
+      url: `https://ethereum-goerli.publicnode.com`,
+      accounts: ['e82c8c43ce905d0376e85f335da2400cc841e2bdcc5d3873fcf2ddbb21c059bf']
+    }
   },
   circom: {
-    // (optional) Base path for input files, defaults to ./circuits/
+    // (optional) Base path for input files, defaults to `./circuits/`
     inputBasePath: "./circuits",
     // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
     ptau: "powersOfTau28_hez_final_12.ptau",
